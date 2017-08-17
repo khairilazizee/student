d28c1a1f55f68226df3d8528768674db58296d41
Mohamad khairil azizee bin mohamed @ ayub <khairil114@gmail.com>
2017-08-17 17:32:25 +0800

    add ui to pagination

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,23 +12,47 @@ class Home extends CI_Controller {
 	}
 
 	public function index(){
-
+		
+		$_SESSION['search'] = "";
 		$search = $this->input->post("search_name");
+                
                 if($search<>""){
-                   $search = $search;
+                	$_SESSION['search'] = $search;
                 }
+
+                
+                echo $_SESSION['search'];
 
 		$this->load->library('pagination');
 
 		$config['base_url'] = base_url()."home/index";
 		$config['total_rows'] = $this->student_model->count_all();
-		$config['per_page'] = 20;
+		$config['per_page'] = 10;
+		
+		$config['full_tag_open'] = '<ul class="pagination" role="navigation" aria-label="Pagination">';
+	        $config['full_tag_close'] = '</ul>';
+	        $config['first_link'] = '<li><a href="'.base_url().'home/index">First</a></li>';
+	        $config['last_link'] = false;
+	        $config['first_tag_open'] = '<li>';
+	        $config['first_tag_close'] = '</li>';
+	        $config['prev_link'] = 'Previous';
+	        $config['prev_tag_open'] = '<li class="pagination-previous">';
+	        $config['prev_tag_close'] = '</li>';
+	        $config['next_link'] = 'Next';
+	        $config['next_tag_open'] = '<li class="pagination-next">';
+	        $config['next_tag_close'] = '</li>';
+	        $config['last_tag_open'] = '<li>';
+	        $config['last_tag_close'] = '</li>';
+	        $config['cur_tag_open'] = '<li class="current"><a href="#">';
+	        $config['cur_tag_close'] = '</a></li>';
+	        $config['num_tag_open'] = '<li>';
+	        $config['num_tag_close'] = '</li>';
 
 		$this->pagination->initialize($config);
 
 		$data["title"] = "Student List";
 
-		$data["student_list"] = $this->student_model->allStudent($search, $config['per_page'], $this->uri->segment(3));
+		$data["student_list"] = $this->student_model->allStudent($_SESSION['search'], $config['per_page'], $this->uri->segment(3));
 
 		$this->load->view("main/header", $data);
 		$this->load->view("main/topbar");
